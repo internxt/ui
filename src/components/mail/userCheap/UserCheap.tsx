83b989fcb107df6a6e57425e6d158f0a32e8a53f
Rafa <rafijv@gmail.com>
2024-03-05 11:30:00 +0100

    feat: UserCheap component

---
+++ src/components/mail/userCheap/UserCheap.tsx
@@ -0,0 +1,14 @@
+import { Avatar } from '../../avatar/Avatar';
+import { EmailProps } from '../mocks';
+
+export const UserCheap = ({ to }: Pick<EmailProps, 'to'>) => {
+  return (
+    <div className="flex bg-white max-w-[256px] w-full border gap-2 items-center border-gray-1 rounded-lg z-20 flex-row p-2">
+      <Avatar diameter={40} fullName={to[0].name} src={to[0].avatar} />
+      <div className="flex flex-col gap-1">
+        <p className="truncate font-medium">{to[0].name}</p>
+        <p className="text-sm text-gray-50 truncate">{to[0].email}</p>
+      </div>
+    </div>
+  );
+};
