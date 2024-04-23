937bf13b1e06617cb0d65605bf5a5fee4a72453e
Rafa <rafijv@gmail.com>
2024-04-23 15:43:00 +0200

    feat: add timeout in MailView to mock the API call

--- src/components/mail/mailView/MailView.tsx
+++ src/components/mail/mailView/MailView.tsx
@@ -1,15 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { emailMocks, EmailProps, updateEmailReadStatus } from '../mocks';
 import { Preview } from '../preview/Preview';
 import { Sidenav } from '../sidenav/Sidenav';
 import { Tray } from '../tray/Tray';
 
 export const MailView = () => {
-  const mails = emailMocks.emailsWithMultipleRecipients;
+  const mockedMails = emailMocks.emailsWithMultipleRecipients;
+  const [mails, setMails] = useState<EmailProps[]>([]);
+  const [isFetchingMails, setIsFetchingMails] = useState<boolean>(false);
   const [mailSelected, setMailSelected] = useState<EmailProps>();
   const [checked, setChecked] = useState<boolean>(false);
   const [activeEmail, setActiveEmail] = useState<string | undefined>();
   const [selectedEmails, setSelectedEmails] = useState<string[]>([]);
+
+  useEffect(() => {
+    setIsFetchingMails(true);
+    setTimeout(() => {
+      setMails(mockedMails);
+      setIsFetchingMails(false);
+    }, 1500);
+  }, []);
 
   const handleOnMessageClicked = (id: string) => {
     const selectedMail = mails.filter((mail) => id === mail.id)[0];
@@ -35,6 +45,7 @@ export const MailView = () => {
       <Tray
         mails={mails}
         activeEmail={activeEmail as string}
+        loading={isFetchingMails}
         checked={checked}
         handleSelectAll={handleSelectAll}
         selectedEmails={selectedEmails}
