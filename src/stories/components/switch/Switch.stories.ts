546091c8fd45e8e4b88e53e58acbc457228f14dd
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-09-19 11:48:18 +0200

    fix: remove comments

--- src/stories/components/switch/Switch.stories.ts
+++ src/stories/components/switch/Switch.stories.ts
@@ -1,24 +1,19 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { SwitchComponent } from '../../../components/switch/Switch';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta: Meta<typeof SwitchComponent> = {
   title: 'Components/Switch',
   component: SwitchComponent,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {},
 };
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Medium: Story = {
   args: {
     disabled: false,
