c338d487119ad5f6dc602e64a063251dd817c230
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-09-19 07:40:22 +0200

    fix: remove comments and update max-len rule config

--- src/stories/components/radio-button/RadioButton.stories.ts
+++ src/stories/components/radio-button/RadioButton.stories.ts
@@ -2,6 +2,7 @@ import type { Decorator, Meta, StoryObj } from '@storybook/react';
 import { useArgs } from '@storybook/preview-api';
 import { RadioButton } from '../../../components/radio-button/RadioButton';
 
+// Simulating onClick event for Radio Button (check or uncheck the button)
 const onClick: Decorator = (Story, context) => {
   const [{ checked }, setArgs] = useArgs();
 
@@ -14,18 +15,14 @@ const onClick: Decorator = (Story, context) => {
   });
 };
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta: Meta<typeof RadioButton> = {
   title: 'Components/Radio Button',
   component: RadioButton,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
   decorators: [onClick],
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
     onClick: { action: 'clicked' },
   },
@@ -34,7 +31,6 @@ const meta: Meta<typeof RadioButton> = {
 export default meta;
 type Story = StoryObj<typeof RadioButton>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const RadioButtonChecked: Story = {
   //
 };
