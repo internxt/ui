1cce87ed010166b4d1c9824daf52dc5342251a1b
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-09-27 10:47:40 +0200

    chore(deps): use testing-library instead of react-test-renderer

--- src/components/radio-button/__test__/RadioButton.test.tsx
+++ src/components/radio-button/__test__/RadioButton.test.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { describe, expect, it, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { RadioButton } from '../RadioButton';
-import renderer from 'react-test-renderer';
 
 describe('Radio Button component', () => {
   it('Radio button onClick should work correctly', () => {
@@ -15,22 +14,22 @@ describe('Radio Button component', () => {
   });
 
   it('Radio Button component should render correctly', () => {
-    const radioButton = renderer.create(<RadioButton checked={false} onClick={() => undefined} />).toJSON();
+    const radioButton = render(<RadioButton checked={false} onClick={() => undefined} />);
     expect(radioButton).toMatchSnapshot();
   });
 
   it('Checked Radio Button component should render correctly', () => {
-    const radioButton = renderer.create(<RadioButton checked onClick={() => undefined} />).toJSON();
+    const radioButton = render(<RadioButton checked onClick={() => undefined} />);
     expect(radioButton).toMatchSnapshot();
   });
 
   it('Disabled Radio Button component should render correctly', () => {
-    const radioButton = renderer.create(<RadioButton disabled checked={false} onClick={() => undefined} />).toJSON();
+    const radioButton = render(<RadioButton disabled checked={false} onClick={() => undefined} />);
     expect(radioButton).toMatchSnapshot();
   });
 
   it('Disabled and Checked Radio Button component should render correctly', () => {
-    const radioButton = renderer.create(<RadioButton disabled checked={false} onClick={() => undefined} />).toJSON();
+    const radioButton = render(<RadioButton disabled checked={false} onClick={() => undefined} />);
     expect(radioButton).toMatchSnapshot();
   });
 });
--- src/components/spinner/__test__/Spinner.test.tsx
+++ src/components/spinner/__test__/Spinner.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { describe, expect, it } from 'vitest';
 import Spinner from '../Spinner';
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
 
 describe('Spinner component', () => {
   it('Primary button should render correctly', () => {
-    const spinner = renderer.create(<Spinner size={20} />).toJSON();
+    const spinner = render(<Spinner size={20} />);
     expect(spinner).toMatchSnapshot();
   });
 });
