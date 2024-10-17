fa32c669f3f028b69235f145562eab552ac72103
Rafa <rafijv@gmail.com>
2024-10-17 09:13:00 +0200

    styles: tray messages

--- src/components/mail/tray/Tray.tsx
+++ src/components/mail/tray/Tray.tsx
@@ -23,54 +23,52 @@ export const Tray = ({
   activeEmail,
   handleSelectAll,
   onMailSelected,
-}: TrayProps) => {
-  return (
-    <div className="flex flex-col w-full max-w-[400px] h-screen border border-gray-5">
-      <div className="flex flex-col gap-3 pt-6 pb-3 px-5 w-full">
-        <Input variant="search" className="bg-gray-5 rounded-lg" placeholder="Search" />
-        <div className="flex flex-row justify-between w-full gap-2.5">
-          <div className="flex flex-row gap-3">
-            <div className="flex flex-row gap-1 items-center">
-              <Checkbox
-                indeterminate={selectedEmails.length > 0 && selectedEmails.length < emailMocks.basicEmails.length}
-                checked={checked}
-                onClick={handleSelectAll}
-              />
-              <CaretDown size={14} className="text-gray-60" weight="fill" />
-            </div>
-            <p className="text-xl font-semibold text-gray-100">Inbox</p>
+}: TrayProps) => (
+  <div className="flex flex-col w-full max-w-[400px] h-screen border border-gray-5">
+    <div className="flex flex-col gap-3 pt-6 pb-3 px-5 w-full">
+      <Input variant="search" className="bg-gray-5 rounded-lg" placeholder="Search" />
+      <div className="flex flex-row justify-between w-full gap-2.5">
+        <div className="flex flex-row gap-3">
+          <div className="flex flex-row gap-1 items-center">
+            <Checkbox
+              indeterminate={selectedEmails.length > 0 && selectedEmails.length < emailMocks.basicEmails.length}
+              checked={checked}
+              onClick={handleSelectAll}
+            />
+            <CaretDown size={14} className="text-gray-60" weight="fill" />
           </div>
-          <div className="flex flex-row gap-4">
-            <FunnelSimple size={24} className="text-gray-100" />
-            <DotsThreeVertical size={24} className="text-gray-100" />
-          </div>
+          <p className="text-xl font-semibold text-gray-100">Inbox</p>
+        </div>
+        <div className="flex flex-row gap-4">
+          <FunnelSimple size={24} className="text-gray-100" />
+          <DotsThreeVertical size={24} className="text-gray-100" />
         </div>
       </div>
-      <div className="border w-full px-5 flex border-gray-10" />
-      <div className="overflow-y-scroll">
-        {loading ? (
-          <>
-            {Array(8)
-              .fill(0)
-              .map((_, index) => (
-                <div key={index} className="flex flex-col gap-2">
-                  <MessageSkeleton />
-                </div>
-              ))}
-          </>
-        ) : (
-          <>
-            {mails.map((email) => (
-              <div key={email.id} className="flex items-center w-full flex-col">
-                <Message email={email} active={checked || activeEmail === email.id} onClick={onMailSelected} />
-                <div className="w-full flex px-5">
-                  <div className="border w-full border-gray-10" />
-                </div>
+    </div>
+    <div className="border w-full px-5 flex border-gray-10" />
+    <div className="overflow-y-scroll">
+      {loading ? (
+        <>
+          {Array(8)
+            .fill(0)
+            .map((_, index) => (
+              <div key={index} className="flex flex-col gap-2">
+                <MessageSkeleton />
               </div>
             ))}
-          </>
-        )}
-      </div>
+        </>
+      ) : (
+        <>
+          {mails.map((email) => (
+            <div key={email.id} className="flex items-center w-full flex-col">
+              <Message email={email} active={checked || activeEmail === email.id} onClick={onMailSelected} />
+              <div className="w-full flex px-5">
+                <div className="border w-full border-gray-10" />
+              </div>
+            </div>
+          ))}
+        </>
+      )}
     </div>
-  );
-};
+  </div>
+);
--- src/components/mail/tray/components/Message.tsx
+++ src/components/mail/tray/components/Message.tsx
@@ -7,30 +7,28 @@ interface MessageProps {
   active?: boolean;
 }
 
-export const Message = ({ email, active, onClick }: MessageProps) => {
-  return (
-    <button
-      onClick={() => onClick(email.id)}
-      className={`flex flex-col text-left gap-2 w-full py-3 px-5 ${active ? 'bg-primary' : ''}`}
-    >
-      <div className="flex flex-row w-full gap-2">
-        <Avatar fullName={email.from.name} src={email.from.avatar} size={'xxs'} className={active ? 'bg-white' : ''} />
-        <div className="flex flex-col w-full">
-          <div className={`flex flex-row w-full justify-between ${active ? 'text-white' : ''}`}>
-            <div className="flex flex-row gap-1 w-full max-w-[150px] items-center">
-              {email.read && <div className="h-2 w-2 rounded-full bg-primary" />}
-              <p className="font-semibold truncate">{email.from.name}</p>
-            </div>
-            <div>
-              <p className={`text-sm font-medium ${active ? 'text-white/80' : 'text-gray-80'}`}>
-                {formatTimestamp(email.timestamp as string)}
-              </p>
-            </div>
+export const Message = ({ email, active, onClick }: MessageProps) => (
+  <button
+    onClick={() => onClick(email.id)}
+    className={`flex flex-col text-left gap-2 w-full py-3 px-5 ${active ? 'bg-primary' : ''}`}
+  >
+    <div className="flex flex-row w-full gap-2">
+      <Avatar fullName={email.from.name} src={email.from.avatar} size={'xxs'} className={active ? 'bg-white' : ''} />
+      <div className="flex flex-col w-full">
+        <div className={`flex flex-row w-full justify-between ${active ? 'text-white' : ''}`}>
+          <div className="flex flex-row gap-1 w-full max-w-[150px] items-center">
+            {email.read && <div className="h-2 w-2 rounded-full bg-primary" />}
+            <p className="font-semibold truncate">{email.from.name}</p>
           </div>
-          <p className={`text-sm font-semibold ${active ? 'text-white' : ''}`}>{email.subject}</p>
-          <p className={`text-sm ${active ? 'text-white/80' : 'text-gray-80'}`}>{email.body}</p>
+          <div>
+            <p className={`text-sm font-medium ${active ? 'text-white/80' : 'text-gray-80'}`}>
+              {formatTimestamp(email.timestamp as string)}
+            </p>
+          </div>
         </div>
+        <p className={`text-sm font-medium ${active ? 'text-white' : ''}`}>{email.subject}</p>
+        <p className={`text-sm ${active ? 'text-white/80' : 'text-gray-80'}`}>{email.body}</p>
       </div>
-    </button>
-  );
-};
+    </div>
+  </button>
+);
