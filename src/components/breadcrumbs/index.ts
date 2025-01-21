cc771fd56287d80a37a77f2b07aab2ba9df2c6b0
Jonatan Pérez <jonatan@internxt.com>
2025-01-21 12:24:31 +0100

    update export types

--- src/components/breadcrumbs/index.ts
+++ src/components/breadcrumbs/index.ts
@@ -1,2 +1,3 @@
 export { default as Breadcrumbs } from './Breadcrumbs';
 export type { BreadcrumbsProps } from './Breadcrumbs';
+export type { BreadcrumbItemData, BreadcrumbsMenuProps } from './BreadcrumbsItem';
