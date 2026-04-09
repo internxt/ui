fb8703f7bfdb60f2e9e9abbdf560691c21d8953e
jaaaaavier <javipiagui13@gmail.com>
2026-04-09 15:45:20 +0200

    Revert "[-]:fix/Copyable component"

--- src/components/input/copyable/Copyable.tsx
+++ src/components/input/copyable/Copyable.tsx
@@ -51,7 +51,7 @@ const Copyable = ({
       <p className={`${classText}`}>{text}</p>
       <Tooltip
         className="ml-6"
-        popsFrom="top"
+        popsFrom="bottom"
         title={justCopied ? copiedText : copyToClipboardText}
         delayInMs={justCopied ? 500 : undefined}
       >
